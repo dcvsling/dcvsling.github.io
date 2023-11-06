04a5ee534692c428846f1feaab3fb54cac47dd05
Kevin Cheng <dcvsling@gmail.com>
2023-11-07 07:34:29 +0800

    rollback

--- src/layout/auth/login.ts
+++ src/layout/auth/login.ts
@@ -10,9 +10,7 @@ import { Subscription, tap } from 'rxjs';
   standalone: true,
   selector: 'login',
   template: `
-  <form *ngIf="!isAuthenticated else profiles" [action]="accessor.authUrl" method="POST">
-    <input type="submit" value="Login" />
-  </form>
+  <a *ngIf="!isAuthenticated else profiles" [href]="accessor.authUrl">Login</a>
   <ng-template #profiles>
     <p>{{ user()?.name }}</p>
   </ng-template>
