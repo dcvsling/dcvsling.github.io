1a98468b4b671946147a84d2adcb8796074d075e
Kevin Cheng <dcvsling@gmail.com>
2023-11-07 15:22:46 +0800

    fix auth redirect

--- src/layout/notfound.ts
+++ src/layout/notfound.ts
@@ -9,19 +9,6 @@ import { Router } from '@angular/router';
 export class NotFound {
   constructor(private router: Router) { }
   ngOnInit(): void {
-    const [_, queryString]= this.router.url.split('?');
-    if(!queryString || queryString.length === 0)
-      this.router.navigate([]);
-    const [[p, pv], [q, qv]] = queryString.split('&').map(kvp => kvp.split('='));
-    if(p === 'p' && q === 'q')
-      this.router.navigate(
-        [pv.split('/')], {
-          queryParams: qv.split('~and~')
-            .map(qkp => qkp.split('='))
-            .reduce((ctx, qkp) => {
-              ctx[qkp[0]] = qkp[1];
-              return ctx;
-            },
-          {} as { [key: string]: any }) });
+    this.router.navigate([]);
   }
 }
